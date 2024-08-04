4daab23ae965948073fddbcb4ae41f7e384206dd
RCH <richiehartonosan@gmail.com>
2024-08-04 22:18:52 +0700

    Tanpa navbar

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
           // Tema aplikasi
           // textTheme: GoogleFonts.istokWebTextTheme(Theme.of(context).textTheme),
           ),
-      home: NavBar(),
+      home: SignInPage(),
     );
   }
 }
